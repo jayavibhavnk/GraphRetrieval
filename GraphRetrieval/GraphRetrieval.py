612bda86665dc1e5d3ae39c78aed3cb00561320e
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-13 16:40:51 +0530

    Update GraphRetrieval.py

--- GraphRetrieval/GraphRetrieval.py
+++ GraphRetrieval/GraphRetrieval.py
@@ -307,6 +307,39 @@ class GraphRAG():
         print("Graph created Successfully!")
 
 
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question,
+in its original language.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""  # noqa: E501
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+def _format_chat_history(chat_history: List[Tuple[str, str]]) -> List:
+    buffer = []
+    for human, ai in chat_history:
+        buffer.append(HumanMessage(content=human))
+        buffer.append(AIMessage(content=ai))
+    return buffer
+
+_search_query = RunnableBranch(
+    # If input includes chat_history, we condense it with the follow-up question
+    (
+        RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
+            run_name="HasChatHistoryCheck"
+        ),  # Condense follow-up question and chat into a standalone_question
+        RunnablePassthrough.assign(
+            chat_history=lambda x: _format_chat_history(x["chat_history"])
+        )
+        | CONDENSE_QUESTION_PROMPT
+        | ChatOpenAI(temperature=0)
+        | StrOutputParser(),
+    ),
+    # Else, we have no chat history, so just pass through the question
+    RunnableLambda(lambda x : x["question"]),
+)
+
+
 class KnowledgeRAG():
     def __init__(self):
         self.hybrid = False
