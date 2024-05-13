a1d6e6ac37fefac7477f8ab89b69b2208762b750
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-13 16:06:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GRaphRetrieval',
+    version='minus2',
+    description='Graph retrieval',
+    long_description='Graph retrieval',
+    author='JVNK',
+    author_email='jaya11vibhav@gmail.com',
+    url='https://github.com/jayavibhavnk/GraphRetrieval',
+    packages=find_packages(),
+    install_requires=[
+      'langchain_openai',
+      'langchain',
+      'sentence_transformers'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+)
