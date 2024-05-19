2c66243ac4e238461be117ff1b4c28ce593550f9
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-19 13:26:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='GraphRetrieval',
     version='0.1.1',
     description='Graph retrieval',
-    long_description=(this_directory / "README.md").read_text(),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     author='JVNK',
     author_email='jaya11vibhav@gmail.com',
     url='https://github.com/jayavibhavnk/GraphRetrieval',
