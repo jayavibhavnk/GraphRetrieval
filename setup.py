6e59038004ba10d77128ebe884632856321e9e81
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-19 13:51:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='GraphRetrieval',
