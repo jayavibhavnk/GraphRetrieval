546319041c8aa9a23717daf6363494fc3ada499e
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-19 13:52:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.1',
     description='Graph retrieval',
     long_description=long_description,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     author='JVNK',
     author_email='jaya11vibhav@gmail.com',
     url='https://github.com/jayavibhavnk/GraphRetrieval',
