45c6003b88a598bb25ce346f5278fd8b7ba87770
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-20 14:43:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ This `README.md` provides an overview of the GraphRetrieval library, installatio
 
 setup(
     name='GraphRetrieval',
-    version='0.1.3',
+    version='0.1.4',
     description='Graph retrieval',
     long_description=long_description,
     long_description_content_type='text/markdown',
