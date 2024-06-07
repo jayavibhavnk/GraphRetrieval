2f9bc272ffafcca39b3d7a7e69b837dd39e6c4b1
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-06-07 14:30:00 +0530

    Update __init__.py

--- GraphRetrieval/__init__.py
+++ GraphRetrieval/__init__.py
@@ -1,8 +1,10 @@
 # Importing objects from modules
-from .GraphRetrieval import GraphRAG, KnowledgeRAG, ImageGraphRAG
+from .graph_retrieval.graph_rag import GraphRAG
+from .graph_retrieval.knowledge_rag import KnowledgeRAG
+from .graph_retrieval.image_graph_rag import ImageGraphRAG
+from .version import __version__
 
 # Defining the public API
 __all__ = ['GraphRAG', 'KnowledgeRAG', 'ImageGraphRAG']
 
-# Version information
-__version__ = '0.1.4'
+__version__ = '0.2.2'
